04ca5234417cd8438fe5a2e6b3e5139dd47daafc
Giacomo Fulvi <giacomofulvi@MBP-Summer6.local>
2024-03-15 10:07:19 +0100

    updated scripts for Caesar and Joseph

--- Caesar_bot_8523.sh
+++ Caesar_bot_8523.sh
@@ -1,142 +1,125 @@
 #!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+RESET='\033[0m'
+ORANGE='\033[38:5:208m'
+
+UKNOWN="[????]"
+CAESAR="[CaesarBot]"
+
 clear
+
+print_animated() {
+    local message=$1
+	local color=$2
+	printf "%b" "$color"
+    while IFS= read -r -n1 var;
+	do
+        printf '%s' "$var"
+        sleep 0.015
+    done <<< "$message"
+	printf "%b" "$RESET"
+}
 
 case "$1" in
     "")
-        str="[???]: Jmv~mv}|i {quxi|qki kzmi|}zi4 qw {wvw Kim{iz m y}m{|w qt uqw i{{q{|mv|m Rw{mxp"
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$UKNOWN" "$RED"
+        str=": Jmv~mv}|i {quxi|qki kzmi|}zi4 qw {wvw Kim{iz m y}m{|w qt uqw i{{q{|mv|m Rw{mxp"
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[???]: *Ahem* *ahem* devi scusarmi, non sono abituato a parlare la vostra lingua, ad ogni modo..."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$UKNOWN" "$RED"
+        str=": *Ahem* *ahem* devi scusarmi, non sono abituato a parlare la vostra lingua, ad ogni modo..."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Caesar]: Piacere di conoscerti curiosa creatura, il mio nome è Kim{iz, addetto alla cifratura dei dati del laboratorio."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$CAESAR" "$GREEN"
+        str=": Piacere di conoscerti curiosa creatura, il mio nome è Kim{iz, addetto alla cifratura dei dati del laboratorio."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Caesar]: Anche se dovrei dire ex addetto alla cifratura visto che a breve verrò {w{|q|}q|w da una mia versione più aggiornata ç.ç"
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$CAESAR" "$GREEN"
+        str=": Anche se dovrei dire ex addetto alla cifratura visto che a breve verrò {w{|q|}q|w da una mia versione più aggiornata ç.ç"
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Caesar]: Ma non perdiamo tempo in {mv|qumv|itq{uq, il mio test consisterà nella decifratura di alcune frasi, un paio dovresti già averle con te se non sbaglio..."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$CAESAR" "$GREEN"
+        str=": Ma non perdiamo tempo in {mv|qumv|itq{uq, il mio test consisterà nella decifratura di alcune frasi, un paio dovresti già averle con te se non sbaglio..."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Caesar]: Assieme alle due frasi dovresti anche avere una chiave con te, giusto?"
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$CAESAR" "$GREEN"
+        str=": Assieme alle due frasi dovresti anche una chiave con te, giusto?"
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Caesar]: Bene come prima cosa direi di mettere insieme quello che hai, per poi passare al vero e proprio test..."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$CAESAR" "$GREEN"
+        str=": Bene come prima cosa direi di mettere insieme quello che hai, per poi passare al vero e proprio test..."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Caesar]: Prima di andare lasciami spiegare in cosa consisterà."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$CAESAR" "$GREEN"
+        str=": Prima di andare lasciami spiegare in cosa consisterà."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Caesar]: Una volta decifrata la frase dovrai inserirla all'interno di un file, ma non un file qualunque, dovrà avere un nome specifico che dovrai trovare."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$CAESAR" "$GREEN"
+        str=": Una volta decifrata la frase dovrai inserirla all'interno di un file, ma non un file qualunque, dovrà avere un nome specifico che dovrai trovare."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Caesar]: Il nome inoltre dovrà essere a sua volta cifrato. Una volta fatto rivolgiti pure al mio assistente ex lui ti dirà come xzw{mo}qzm..."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$CAESAR" "$GREEN"
+        str=": Il nome inoltre dovrà essere a sua volta cifrato. Una volta fatto rivolgiti pure al mio assistente e lui ti dirà come xzw{mo}qzm..."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Kim{iz]: Umano questo è il mio ultimo enigma! prendilo!"
+        print_animated "[Kim{iz]" "$ORANGE"
+        str=": Umano questo è il mio ultimo enigma! Prendilo!"
+        print_animated "$str" "$WHITE"
 
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
-
+        echo
         echo
     ;;
     *)
         echo
         echo "Errore: numero di argomenti non valido."
-        echo "Puoi chiamare Caesar_bot_8523 solamente con: \"sh Caesar_bot_8523.sh\""
+        echo "Puoi chiamare CaesarBot solamente con: \"sh CaesarBot.sh\""
         echo
         exit 1
     ;;
--- Rw{mxp.sh
+++ Rw{mxp.sh
@@ -1,217 +1,217 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+RESET='\033[0m'
+ORANGE='\033[38:5:208m'
+
+UKNOWN="[????]"
+JOSEPH="[Joseph]"
+
+clear
+
+print_animated() {
+    local message=$1
+	local color=$2
+	printf "%b" "$color"
+    while IFS= read -r -n1 var;
+	do
+        printf '%s' "$var"
+        sleep 0.015
+    done <<< "$message"
+	printf "%b" "$RESET"
+}
+
+
 case $1 in
     "")
-        str="[???]: Salve, io sono Joseph, l'assitente di Caesar."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$UKNOWN" "$RED"
+        str=": Salve, io sono Joseph, l'assitente di Caesar."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Joseph]: Salve, io sono Joseph, l'assitente di Caesar."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Dal momento che Caesar sta per essere congedato e io dovrò prendere il suo posto, il creatore mi ha incaricato di sottoporre il prossimo test per dimostrare le mie abilità."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Joseph]: Dal momento che Caesar sta per essere congedato e io dovrò prendere il suo posto, il creatore mi ha incaricato di sottoporre il prossimo test per dimostrare le mie abilità."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
-
-        echo
-        sleep 0.3
-        echo
-
-        str="[Joseph]: Percui ascoltami attentamente adesso. Nzella cartella data troverai due set di operazioni: \"first_op\" e \"second_op\"."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Per cui ascoltami attentamente adesso. Nella cartella "
+        print_animated "$str" "$WHITE"
+        print_animated "data" "$YELLOW"
+        str=" troverai due set di operazioni: "
+        print_animated "$str" "$WHITE"
+        print_animated "first_op" "$YELLOW"
+        print_animated " e " "$WHITE"
+        print_animated "second_op" "$YELLOW"
+        print_animated "." "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-
-        str="[Joseph]: Il tuo compito sarà di modificare i permessi di ciascuno dei file come ti verrà detto dalla documentazione che ti ho lasciato per ciascuna cartella."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Il tuo compito sarà di modificare i permessi di ciascuno dei file come ti verrà detto dalla documentazione che ti ho lasciato per ciascuna cartella."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Joseph]: Una volta fatto questo dovrai fare la somma di tutti permessi di ciascun file."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Una volta fatto questo dovrai fare la somma di tutti permessi di ciascun file."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Joseph]: Arrivati a questo punto avrai sicuramente notato che i file sono numerati con un certo ordine e ciascuno di essi termina con un simbolo di un operazione."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Arrivati a questo punto avrai sicuramente notato che i file sono numerati con un certo ordine e ciascuno di essi termina con un simbolo di un'operazione."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Joseph]: Devi quindi andare ad eseguire anche queste operazioni nell'ordine in cui sono disposte usando come numeri i risultati della somma dei permessi."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Devi quindi andare ad eseguire queste operazioni nell'ordine in cui sono disposte usando come numeri i risultati della somma dei permessi del file in questione."
+        print_animated "$str" "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Joseph]: Ti faccio un piccolo esempio: poni che tu abbia 1_op_*, 2_op_+, 3_op_-, 4_op_: e 5_op_=."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
-
-        echo
-        sleep 0.3
-        echo
-
-        str="[Joseph]: In questo caso farai la moltiplicazione del totale dei permessi di 1_op_+ e 2_op_*. Al risultato sottrarrai il totale dei permessi di 3_op_-."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Ti faccio un piccolo esempio: poni che tu abbia "
+        print_animated "$str" "$WHITE"
+        print_animated "1_op_*" "$YELLOW"
+        print_animated ", " "$WHITE"
+        print_animated "2_op_+" "$YELLOW"
+        print_animated ", " "$WHITE"
+        print_animated "3_op_-" "$YELLOW"
+        print_animated ", " "$WHITE"
+        print_animated "4_op_:" "$YELLOW"
+        print_animated " e " "$WHITE"
+        print_animated "5_op_=" "$YELLOW"
+        print_animated "." "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Joseph]: Al nuovo risultato sottrarrai il totale dei permessi di 4_op_: ed infine andrai a dividere il risultato per il totale dei permessi di 5_op_="
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
-
-        echo
-        sleep 0.3
-        echo
-
-        str="[Joseph]: Una volta arrivato a questo punto prova a scrivere \"man ascii\" e cerca di capire cosa rappresentano quei due numeri (tip usa le freccette per scorrere)"
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$JOSEPH" "$GREEN"
+        str=": In questo caso farai la moltiplicazione del totale dei permessi di "
+        print_animated "$str" "$WHITE"
+        print_animated "1_op_*" "$YELLOW"
+        print_animated " e " "$WHITE"
+        print_animated "2_op_+" "$YELLOW"
+        str=". Al risultato sommerai il totale dei permessi di "
+        print_animated "$str" "$WHITE"
+        print_animated "3_op_-" "$YELLOW"
+        print_animated "." "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Joseph]: Nel caso in cui volessi rileggere questa spiegazione chiamami pure con \"sh Rw{mxp.sh help\""
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Al nuovo risultato sottrarrai il totale dei permessi di "
+        print_animated "4_op_:" "$YELLOW"
+        str=" ed infine andrai a dividere il risultato per il totale dei permessi di "
+        print_animated "5_op_=" "$YELLOW"
+        print_animated "." "$WHITE"
 
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        echo
+        sleep 0.3
+        echo
+
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Una volta arrivato a questo punto prova a scrivere "
+        print_animated "$str" "$WHITE"
+        print_animated "man ascii" "$PURPLE"
+        str=" e cerca di capire cosa rappresentano quei due numeri (usa le freccette per scorrere verso l'alto e il basso)"
+        print_animated "$str" "$WHITE"
+
+        echo
+        sleep 0.3
+        echo
+
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Nel caso in cui volessi rileggere questa spiegazione chiamami pure con "
+        print_animated "$str" "$WHITE"
+        print_animated "sh Rw{mxp.sh help" "$PURPLE"
 
         echo
     ;;
 
     "help")
-        str="[Joseph]: Poni che tu abbia 1_op_*, 2_op_+, 3_op_-, 4_op_: e 5_op_=."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
-
-        echo
-        sleep 0.3
-        echo
-
-        str="[Joseph]: In questo caso farai la moltiplicazione del totale dei permessi di 1_op_* e 2_op_+ . Al risultato sottrarrai il totale dei permessi di 3_op_-."
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$JOSEPH"
+        str=": Poni che tu abbia "
+        print_animated "$str" "$WHITE"
+        print_animated "1_op_*" "$YELLOW"
+        print_animated ", " "$WHITE"
+        print_animated "2_op_+" "$YELLOW"
+        print_animated ", " "$WHITE"
+        print_animated "3_op_-" "$YELLOW"
+        print_animated ", " "$WHITE"
+        print_animated "4_op_:" "$YELLOW"
+        print_animated " e " "$WHITE"
+        print_animated "5_op_=" "$YELLOW"
+        print_animated "." "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Joseph]: Al nuovo risultato sottrarrai il totale dei permessi di 4_op_: ed infine andrai a dividere il risultato per il totale dei permessi di 5_op_="
-
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        print_animated "$JOSEPH" "$GREEN"
+        str=": In questo caso farai la moltiplicazione del totale dei permessi di "
+        print_animated "$str" "$WHITE"
+        print_animated "1_op_*" "$YELLOW"
+        print_animated " e " "$WHITE"
+        print_animated "2_op_+" "$YELLOW"
+        str=". Al risultato sommerai il totale dei permessi di "
+        print_animated "$str" "$WHITE"
+        print_animated "3_op_-" "$YELLOW"
+        print_animated "." "$WHITE"
 
         echo
         sleep 0.3
         echo
 
-        str="[Joseph]: Nel caso in cui volessi rileggere questa spiegazione chiamami pure con \"sh Rw{mxp.sh help\""
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Al nuovo risultato sottrarrai il totale dei permessi di "
+        print_animated "4_op_:" "$YELLOW"
+        str=" ed infine andrai a dividere il risultato per il totale dei permessi di "
+        print_animated "5_op_=" "$YELLOW"
+        print_animated "." "$WHITE"
 
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
+        echo
+        sleep 0.3
+        echo
+
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Nel caso in cui volessi rileggere questa spiegazione chiamami pure con "
+        print_animated "$str" "$WHITE"
+        print_animated "sh Rw{mxp.sh help" "$PURPLE"
 
         echo
     ;;
 
     *)
-        str="[Joseph]: Mi dispiace ma non conosco questa opzione :("
+        print_animated "$JOSEPH" "$GREEN"
+        str=": Mi dispiace ma non conosco questa opzione :("
+        print_animated "$str" "$WHITE"
 
-        while IFS= read -r -n1 var
-        do
-            printf '%s' "$var"
-            sleep 0.015
-        done <<< "$str"
         echo
     ;;
 esac
